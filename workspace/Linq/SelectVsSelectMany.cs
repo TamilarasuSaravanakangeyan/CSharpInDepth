ae77ab0ef73d594e3ba07e34ec29736f61ad0793
Tamil1503 <tamilzarasus@gmail.com>
2022-11-18 02:32:00 +0530

    C# select vs select many code sample

---
+++ workspace/Linq/SelectVsSelectMany.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace workspace.Linq
+{
+    class Consumer
+    {
+        public string? Name { get; set; }
+        public List<string>? Skills { get; set; }
+    }
+    internal class SelectVsSelectMany
+    {
+        public void ReturnResponse()
+        {
+            List<Consumer> consumers = new List<Consumer>() { 
+                new Consumer(){ Name="Tamil", Skills= new List<string>(){ ".net","Java"} },
+                new Consumer(){ Name="Swathi", Skills= new List<string>(){ ".net","UI/UX"} }
+            };
+            IEnumerable<List<string>> consumers1 = consumers.Select(x => x.Skills);
+            foreach (var item in consumers1)
+            {
+                foreach (var item1 in item)
+                {
+                    Console.WriteLine(item1);
+                }
+            }
+
+            IEnumerable<string> consumers2 = consumers.SelectMany(x => x.Skills);
+            foreach (var item in consumers2)
+            {
+                Console.WriteLine(item);
+
+            }
+        }
+
+    }
+}
--- workspace/Program.cs
+++ workspace/Program.cs
@@ -1,8 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using workspace.Linq;
+
 public class Welcome
 {
     public static void Main(string[] arg)
     {
         Console.WriteLine("Hello World");
+        SelectVsSelectMany selectVsSelectMany= new SelectVsSelectMany();
+        selectVsSelectMany.ReturnResponse();
+
     }
 }
